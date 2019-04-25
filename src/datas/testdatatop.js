871fd5f901c3eaab6430ce3738d394c7f31601ab
dcnina <dcninaa@gmail.com>
2019-04-25 23:49:58 +0200

    datas

---
+++ src/datas/testdatatop.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        name: "Loic Nottet",
+        like: 320,
+        img: "favorite-heart-button-purple"
+    },
+    {
+        name: "Miel de Montagne",
+        like: 316,
+        img: "favorite-heart-button-purple"
+    },
+    {
+        name: "Angèle",
+        like: 314,
+        img: "favorite-heart-button-purple"
+    },
+  ];
