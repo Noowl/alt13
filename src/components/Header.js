897115bb690912f59670f4567fa32dfe51bf8a6d
Noélie <noeliebravo@laposte.net>
2019-05-02 18:09:42 +0200

    ajout state Header.js pour barre de recherche

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Header extends Component {
+  state = {
+    myArtist : ""
+  }
+
   render(){
     return(
       <div className="header">
@@ -8,7 +12,12 @@ class Header extends Component {
           <h1>alt-13</h1>
         </div>
         <div className="header-search">
-          <input placeholder="trouver un artiste" type="text"/>
+          <input
+            placeholder="trouver un artiste"
+            type="text"
+            required
+            value={this.state.myArtist}
+            onChange={e => this.setState({myArtist: e.target.value})}/>
           <button><img src={require('../assets/search.png')} alt="search icon"/></button>
         </div>
       </div>
