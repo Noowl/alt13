079fbda64fb1941018f96987f4696177565f8754
dcnina <dcninaa@gmail.com>
2019-05-18 10:59:02 +0200

    Push constantmanager

--- src/helpers/ConstantManager.js
+++ src/helpers/ConstantManager.js
@@ -1,2 +1,2 @@
-export const API_KEY = "&apikey=b69c809a255cd65c27192ba85b41fa5d"
+export const API_KEY = "&apikey=ae2b7899c4f8ab311409ae419a87c2b4"
 export const API_URL = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/"
