afc56ecbe95489d219b22c0c61a1859becb7f3e8
Noélie <noeliebravo@laposte.net>
2019-05-19 21:01:05 +0200

    New apikey

--- src/helpers/ConstantManager.js
+++ src/helpers/ConstantManager.js
@@ -1,2 +1,2 @@
-export const API_KEY = "&apikey=ae2b7899c4f8ab311409ae419a87c2b4"
+export const API_KEY = "&apikey=622edcfb80519eececbb1aaedca994dc"
 export const API_URL = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/"
